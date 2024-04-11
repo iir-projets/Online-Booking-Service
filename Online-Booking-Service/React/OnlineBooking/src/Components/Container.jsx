da1d17d6e8f37c0ec447e11dea3877e0e3e892ed
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-04-11 21:17:58 +0000

    React patch 2: +added product container +added products page +added routing

---
+++ Online-Booking-Service/React/OnlineBooking/src/Components/Container.jsx
@@ -0,0 +1,16 @@
+import img from "../assets/support.png";
+
+function Container(props) {
+  return (
+    <div className="flex-col border-2 w-56 h-72 shadow-xl items-center justify-center">
+      <div className="flex justify-center"> 
+        <img src={img} alt="" className="w-44" />
+      </div>
+      <p className="text-center font-mono text-2xl">Title</p>
+      <p className="mt-2 font-mono text-l">Rating: ⭐⭐⭐⭐⭐</p>
+      <button className="ml-12 w-28 border-2 p-2 m-3 bg-red-400 text-white font-mono font-bold duration-1000 rounded-2xl hover:text-red-400 hover:bg-white hover:border-red-400 hover:translate-x-4">Book Now</button>
+    </div>
+  );
+}
+
+export default Container;
--- Online-Booking-Service/React/OnlineBooking/src/main.jsx
+++ Online-Booking-Service/React/OnlineBooking/src/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter , RouterProvider} from 'react-router-dom';
 import Home from './Home.jsx';
+import Container from './Components/Container.jsx';
+import Products from './Components/Products.jsx';
 
 
 const router = createBrowserRouter([
   {
     //default to try new features
     path:'/',
+    element: <Products/>
+  },
+  {
+    //default to try new features
+    path:'/container',
+    element: <Container/>
+  },
+  {
+    //default to try new features
+    path:'/home',
     element: <Home/>
   }
 ]);
