a67c52dedb128e9d04ef3a9e18757a1b696983f2
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-04-15 13:16:43 +0100

    check token validity + I added an endpoint that uses my JWT service to check token validity: 	true => token valid 	false => token expired 	modified:   Online-Booking-Service/Spring/MySpringApp/src/main/java/org/example/myspringapp/Controller/UserController.java

--- Online-Booking-Service/Spring/MySpringApp/src/main/java/org/example/myspringapp/Controller/UserController.java
+++ Online-Booking-Service/Spring/MySpringApp/src/main/java/org/example/myspringapp/Controller/UserController.java
@@ -1,12 +1,14 @@
 package org.example.myspringapp.Controller;
 
 import org.example.myspringapp.Model.User;
+import org.example.myspringapp.Service.JWTUtils;
 import org.example.myspringapp.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Map;
 
 @RestController
@@ -14,6 +16,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    JWTUtils jwtUtils;
 
     @PostMapping("/add")
     public ResponseEntity<Map<String, String>> addUser(@RequestBody User user) {
@@ -50,5 +54,9 @@ public class UserController {
     @PostMapping("/authenticate")
     public Map<String, String> authenticateUser(@RequestBody Map<String, String> credentials) {
         return userService.authenticateUser(credentials);
+    }
+    @GetMapping("/checkToken")
+    public boolean checkToken(@RequestParam("token") String token) {
+        return !jwtUtils.extractExpiration(token).before(new Date());
     }
 }
